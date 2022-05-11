3ac712d341b68dc9157e1367a450396ba3f27c77
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-05-11 22:12:28 +0300

    Create project for work with SQLITE in future (CS database)

---
+++ src/CS_assign/CS_Load.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Teigha.DatabaseServices;
+using Teigha.Runtime;
+using Teigha.Geometry;
+using HostMgd.ApplicationServices;
+using HostMgd.EditorInput;
+
+using Platform = HostMgd;
+using PlatformDb = Teigha;
+
+namespace NanoCAD_GIS
+{
+    public class CS_Load
+    {
+        public string selected_code = null;
+        public CS_Load()
+        {
+
+        }
+    }
+}
