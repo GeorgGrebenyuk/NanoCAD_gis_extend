65c5eff9f545ba2983603b4f0b9339c7e1f5d7c6
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-05-11 22:12:43 +0300

    Static classes as tools and containers

---
+++ src/MainApp/GeneralData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Teigha.DatabaseServices;
+using Teigha.Runtime;
+using Teigha.Geometry;
+using HostMgd.ApplicationServices;
+using HostMgd.EditorInput;
+
+using Platform = HostMgd;
+using PlatformDb = Teigha;
+
+namespace NanoCAD_GIS
+{
+    /// <summary>
+    /// Static class as container for constants
+    /// </summary>
+    public static class GeneralData
+    {
+        public static Database nc_db;
+        public static Document nc_doc;
+        public static string path_to_db = $@"C:\Users\{Environment.UserName}\AppData\Local\proj\proj.db";
+        public static string CS_code = null;
+    }
+    public static class Tools
+    {
+        public static void GettingUserInput (string FirstMessage, ref string OutPutInfo)
+        {
+            PromptStringOptions opts = new PromptStringOptions(FirstMessage);
+            opts.AllowSpaces = false;
+            PromptResult pr = GeneralData.nc_doc.Editor.GetString(opts);
+            if (PromptStatus.OK == pr.Status)
+            {
+                OutPutInfo = pr.StringResult;
+            }
+            else
+            {
+                GeneralData.nc_doc.Editor.WriteMessage("Повторите еще раз");
+                GettingUserInput(FirstMessage, ref OutPutInfo);
+            }
+
+        }
+    }
+}
